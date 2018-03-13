f63fe5e728c9859739024efbaf779b229e013cec
sh <sonhgc00016@gmail.com>
2018-03-13 09:30:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -77,6 +77,7 @@ port.on('data', data => {
   if (catchedData && trackingId) {
     io.sockets.emit(trackingId, catchedData);
     console.log(`Sent ${catchedData} to ${trackingId}`);
+    io.sockets.emit('close');
   }
 });
 
